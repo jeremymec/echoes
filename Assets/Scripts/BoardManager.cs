528fb8f3668c03fe9c995e5334a80cb076bbb220
Jeremy Mechen <jeremymechen1997@gmail.com>
2017-07-06 18:47:55 +1200

    Code fully commented

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour {
 
+    // Array of different types of Tile PREFABS that can be used to create new tiles
     public GameObject[] tiles;
+
+    // 2D Array of Tile GameObjects that make up the current game board
     public GameObject[,] board;
 
 	public void setupScene (int width, int height) {
@@ -13,6 +16,7 @@ public class BoardManager : MonoBehaviour {
 
     void createBoard(int width, int height)
     {
+        // Initializes array with GameObjects, that will be filled with Tiles
         board = new GameObject[width, height];
 
         for (int y = 0; y < height; y++)
@@ -20,10 +24,14 @@ public class BoardManager : MonoBehaviour {
 
             for (int x = 0; x < height; x++)
             {
-                GameObject tile = Instantiate(tiles[1], new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+                // Instantiates tile of the specified type (empty tile) with coords determined by the for loop, and 0 rotiation.  
+                GameObject tile = Instantiate(tiles[0], new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+
+                // Places tile into GameObject array
                 this.board[x, y] = tile;
+
+                // Calls the 'insert' function of the TileScript, which stores it's position in the array
                 tile.SendMessage("insert", new int[2] { x, y });
-                // Debug.Log("Created game object at X: " + x + " and Y: " + y);
             }
 
         }
@@ -33,7 +41,8 @@ public class BoardManager : MonoBehaviour {
     }
 
     void setupMaze()
-    {
+    {   
+        // Creates a new MazeBuilder object for the newly created board fill of empty tiles
         MazeBuilder mazeBuilder = new MazeBuilder(this.board, this.tiles, 0, 0);
     }
 
--- Assets/Scripts/MazeBuilder.cs
+++ Assets/Scripts/MazeBuilder.cs
@@ -7,9 +7,16 @@ public class MazeBuilder : ScriptableObject {
 
     enum Direction { UP, DOWN, LEFT, RIGHT}
 
+    // Array of tiles types, passed in by BoardManager as opposed to dragged in using unity
     GameObject[] tiles;
+
+    // Board array passed in by BoardManager
     GameObject[,] board;
+
+    // Tile to begin building the maze on
     GameObject start;
+
+    // Empty stack utilized by the DFS algorithm
     Stack<GameObject> stack;
 
 	public MazeBuilder(GameObject[,] board, GameObject[] tiles, int startX, int startY)
@@ -19,51 +26,91 @@ public class MazeBuilder : ScriptableObject {
         this.stack = new Stack<GameObject>();
         this.start = board[startX, startY];
 
-        check(this.start, this.stack);
+        check(this.start, this.stack); // Debugging
     }
 
+    /// <summary>
+    /// Takes an empty tile and 'makes a path', i.e. turn it into a traversable floor tile
+    /// </summary>
     void makePath(GameObject target)
     {
+        // Gets coords of target to be replaced
         int[] targetPos = target.GetComponent<TileScript>().arrayPos;
-        Destroy(target);
 
+        // Creates new floor tile and replaces the array reference to the empty tile with this new tile
+        GameObject tile = Instantiate(tiles[1], new Vector3(targetPos[0], targetPos[0], 0), Quaternion.identity) as GameObject;
+        this.board[targetPos[0], targetPos[1]] = tile;
+
+        // Removes the target from the game board
+        Destroy(target);
     }
 
+    /// <summary>
+    /// Recursive function used by DFS to build the maze
+    /// </summary>
+    /// <param name="current">Current tile in DFS</param>
+    /// <param name="stack">The stack, which is needed to step back through maze</param>
     void check(GameObject current, Stack<GameObject> stack)
     {
+        // Check if maze can be continued in EACH direction
         foreach (Direction dir in Enum.GetValues(typeof(Direction)))
         {
-            if (move(current, dir, 1) && (move(current, dir, 2))){
+
+            GameObject testTile = move(current, dir, 1); // Debugging
+            bool test = isEmpty(testTile); // Debugging
+
+            // Stores whether the first AND second tile in the specified direction is empty 
+            bool lookAheadOne = isEmpty(move(current, dir, 1));
+            bool lookAheadTwo = isEmpty(move(current, dir, 2));
+
+            // If both are empty, the maze can continue building
+            if (lookAheadOne && lookAheadTwo){
                 makePath(current);
                 stack.Push(current);
                 check(move(current, dir, 1), stack);
             }
         }
+
+        // If dead end is reached, step back using the stack
         if (stack.Peek() != null)
         {
             check(stack.Pop(), stack);
         }
     }
 
+    /// <summary>
+    /// Checks if a given cell is an empty tile
+    /// </summary>
+    /// <returns>True if empty, false if any other type</returns>
     bool isEmpty(GameObject cell)
     {
         if (cell == null)
         {
             return false;
         }
+
+        // Checks by tag comparison (THERE MIGHT BE A BETTER WAY TO DO THIS)
         if (cell.CompareTag("emptyTile"))
         {
             return true;
         }
+
         return false;
     }
 
+    /// <summary>
+    /// Returns the GameObject ahead by a given number of tiles, in a given direction.
+    /// </summary>
+    /// <param name="cell">The cell to move FROM</param>
+    /// <param name="dir">The direction (using Enum Types)</param>
+    /// <param name="amount">Magnitude of the move</param>
     GameObject move(GameObject cell, Direction dir, int amount)
-    {
+    {   
         GameObject target = null;
         int deltaX = 0;
         int deltaY = 0;
 
+        // Switchcase to calculate deltax and deltay for given direction
         switch (dir)
         {
             case Direction.UP:
@@ -83,8 +130,10 @@ public class MazeBuilder : ScriptableObject {
                 break;
         }
 
+        // This exists to prevent throwing an error when the function checks outside the maze - instead, it will return null.
         try
         {
+            // Gets the cell at the array position corresponding to the movement 
             int[] pos = cell.GetComponent<TileScript>().arrayPos;
             target = board[pos[0] + deltaX, (pos[1] + deltaY)];
         }
